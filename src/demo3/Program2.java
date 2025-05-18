423864ac3c66f786ddc8ae607c8e37f3bc917b8d
rahulchauhan-99 <rahulchuahan5772@gmail.com>
2025-05-18 21:37:19 +0530

    added one more printing statement

--- src/demo3/Program2.java
+++ src/demo3/Program2.java
@@ -1,5 +1,10 @@
 package demo3;
 
 public class Program2 {
+	
+	public static void main(String[] args) {
+		System.out.println("this is program2");
+		System.out.println("this is main method");
+	}
 
 }
